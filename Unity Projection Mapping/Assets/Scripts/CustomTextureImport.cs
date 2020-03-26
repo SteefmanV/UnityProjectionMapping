678ff0e16a09827b97094fcc3e6db56e1feacf5e
Steefman <svolkers99@gmail.com>
2020-03-26 17:11:44 +0100

    Implmeneted custom image importer. User are now able to imopert custom .png & .jpg

---
+++ Unity Projection Mapping/Assets/Scripts/CustomTextureImport.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+public class CustomTextureImport : MonoBehaviour
+{
+    private Material _imageMaterial = null;
+
+    public void ImportCustomFile()
+    {
+        string filePath = searchFileInExplorer();
+        Texture2D texture = loadFileIntoTexture(filePath);
+
+        if (texture != null)
+        {
+            _imageMaterial.SetTexture("_BaseColorMap", texture);
+            Sprite materialThumnail = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f), 100);
+            FindObjectOfType<SelectedUI>().selectNewMatrial(_imageMaterial, materialThumnail);
+        }
+    }
+
+
+    /// <summary>
+    /// Opens a file dialog where the user can select a file. Return the string of the selected file. 
+    /// </summary>
+    private string searchFileInExplorer()
+    {
+        string filePath = "";
+
+        OpenFileDialog fileDialog = new OpenFileDialog();
+        fileDialog.Filter = "Image (*.PNG; *.JPG;)| *.PNG; *.JPG;"; // Show only PNG & JPG files
+        fileDialog.FilterIndex = 1;
+        fileDialog.Multiselect = false; // Allow only 1 image
+
+        if (fileDialog.ShowDialog() == DialogResult.OK)
+            filePath = fileDialog.FileName;
+
+        return filePath;
+    }
+
+
+    /// <summary>
+    /// Loads image from filepath and converts it to a Texture2D
+    /// </summary>
+    private Texture2D loadFileIntoTexture(string pFilePath)
+    {
+        Texture2D texture = null; // Create default texture, size doesn't matter
+
+        if (File.Exists(pFilePath))
+        {
+            byte[] fileData = File.ReadAllBytes(pFilePath);
+            texture.LoadImage(fileData); // Loads image data and resets the texture size
+        }
+
+        return texture;
+    }
+}
+
